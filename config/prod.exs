368572f29b3a3c67f34cee836055a0e7d3ee4953
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-04 17:48:47 -0300

    Updates Endpoint config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,22 +13,32 @@ config :currency_converter, CurrencyConverterWeb.Endpoint, cache_static_manifest
 
 # Do not print debug messages in production
 config :logger, level: :info
-
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :currency_converter, CurrencyConverterWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         ...,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
+host = System.get_env("PHX_HOST") || "example.com"
+port = String.to_integer(System.get_env("PORT") || "4000")
+
+config :currency_converter, CurrencyConverterWeb.Endpoint,
+  url: [scheme: "https", host: host, port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  http: [
+    # Enable IPv6 and bind on all interfaces.
+    # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+    # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+    # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+    ip: {0, 0, 0, 0, 0, 0, 0, 0},
+    port: port
+  ],
+  secret_key_base: secret_key_base
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
