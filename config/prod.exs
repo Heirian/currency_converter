ce7402b35ba4f6a78133e1c24b2edb9010bc0cab
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-04 17:30:10 -0300

    Adds exchangerateapi ket to config prod

--- config/prod.exs
+++ config/prod.exs
@@ -47,3 +47,5 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
+config :currency_converter, CurrencyConverter.Finances,
+  secret_key: System.get_env("EXCHANGERATESAPI_KEY")
