83682573ac348cf282d78a1d1c582814826d9aa8
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-04 11:40:45 -0300

    Adds session feature to handle the authentication flow

--- lib/currency_converter_web/router.ex
+++ lib/currency_converter_web/router.ex
@@ -5,27 +5,22 @@ defmodule CurrencyConverterWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug CurrencyConverter.Guardian.AuthPipeline
+  end
+
   scope "/api", CurrencyConverterWeb do
     pipe_through :api
 
     post "/users", UserController, :register
+    post "/session/new", SessionController, :new
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  scope "/api", CurrencyConverterWeb do
+    pipe_through [:api, :auth]
 
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: CurrencyConverterWeb.Telemetry
-    end
+    post "/session/refresh", SessionController, :refresh
+    post "/session/delete", SessionController, :delete
   end
 
   # Enables the Swoosh mailbox preview in development.
